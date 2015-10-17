28db451d233d7c4659e645debfb865fbf194e4d5
yuanqing <desk@yuanqing.sg>
2015-10-17 21:06:57 +0800

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -1,39 +1,37 @@
 'use strict';
 
 var tmplt = require('..');
+
 var test = require('tape');
 
-test('tmplt(tmpl)(data)', function(t) {
+test('works without `data`', function(t) {
+  t.plan(1);
+  t.equal(tmplt('foo')(), 'foo');
+});
 
-  t.test('works without `data`', function(t) {
-    t.plan(1);
-    t.equal(tmplt('foo')(), 'foo');
+test('interpolates values in `data`', function(t) {
+  t.plan(4);
+  var data = { foo: 'bar' };
+  t.equal(tmplt('{{foo}}')(data), 'bar');
+  t.equal(tmplt('{{ foo}}')(data), 'bar');
+  t.equal(tmplt('{{foo }}')(data), 'bar');
+  t.equal(tmplt('{{ foo }}')(data), 'bar');
+});
+
+test('interpolates nested values in `data`', function(t) {
+  t.plan(1);
+  var data = {
+    foo: 'Hello',
+    bar: {
+      baz: 'World'
+    }
+  };
+  t.equal(tmplt('{{ foo }}, {{ bar.baz }}!')(data), 'Hello, World!');
+});
+
+test('throws if a variable referenced in `tmpl` is not in `data`', function(t) {
+  t.plan(1);
+  t.throws(function() {
+    tmplt('{{ foo }}')({});
   });
-
-  t.test('interpolates values in `data`', function(t) {
-    t.plan(4);
-    var data = { foo: 'bar' };
-    t.equal(tmplt('{{foo}}')(data), 'bar');
-    t.equal(tmplt('{{ foo}}')(data), 'bar');
-    t.equal(tmplt('{{foo }}')(data), 'bar');
-    t.equal(tmplt('{{ foo }}')(data), 'bar');
-  });
-
-  t.test('interpolates nested values in `data`', function(t) {
-    t.plan(1);
-    t.equal(tmplt('{{ foo }}, {{ bar.baz }}!')({
-      foo: 'Hello',
-      bar: {
-        baz: 'World'
-      }
-    }), 'Hello, World!');
-  });
-
-  t.test('throws if a variable referenced in `tmpl` is not in `data`', function(t) {
-    t.plan(1);
-    t.throws(function() {
-      tmplt('{{ foo }}')({});
-    });
-  });
-
 });
